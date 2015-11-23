b0a9fe763fb3d21b821a9af35b5ab041d5aacc9f
gbember <605660475@qq.com>
2015-11-23 18:20:13 +0800

    timer测试

--- timer/timer_test.go
+++ timer/timer_test.go
@@ -8,14 +8,26 @@ import (
 )
 
 func TestTimer(t *testing.T) {
-	timer := NewTimer()
-	timer.AddFun(10*time.Second, func() { log.Println("111111111") })
-	timer.AddFun(5*time.Second, func() { log.Println("222222222") })
-	timer.AddFun(2*time.Second, func() { log.Println("3333333333") })
-	<-timer.T.C
+	//	timer := NewTimer()
+	//	timer.AddFun(10*time.Second, func() { log.Println("111111111") })
+	//	timer.AddFun(5*time.Second, func() { log.Println("222222222") })
+	//	timer.AddFun(2*time.Second, func() { log.Println("3333333333") })
+	//	<-timer.T.C
+	//	timer.Run()
+	//	<-timer.T.C
+	//	timer.Run()
+	//	<-timer.T.C
+	//	timer.Run()
+
+	timer := NewTimerFunQueue()
+	timer.AddAfterTimerFun(10*time.Second, func() { log.Println("111111111") })
+	timer.AddAfterTimerFun(5*time.Second, func() { log.Println("222222222") })
+	timer.AddAfterTimerFun(2*time.Second, func() { log.Println("3333333333") })
+	<-timer.C
 	timer.Run()
-	<-timer.T.C
+	<-timer.C
 	timer.Run()
-	<-timer.T.C
+	<-timer.C
 	timer.Run()
+	timer.Stop()
 }
